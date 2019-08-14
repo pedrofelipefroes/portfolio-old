32d41bb4ee6d35d114894c109c38c8aea59b6dd4
Avenue Code <avenuecode@AC-LP0176-pfroes.local>
2019-08-13 21:26:25 -0300

    Adding Footer component.

---
+++ src/components/footer/footer.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Footer = () => (
+  <footer className="c-footer">
+    <div className="l-container c-footer__container">
+      <a href="www.linkedin.com/in/froesdesign" className="c-footer__link">
+        LinkedIn
+      </a>
+      <a href="mailto:contact@froes.design" className="c-footer__link">
+        Work Inquiries
+      </a>
+      <small>
+        This site was fully designed and developed by me. Check out its{" "}
+        <a href="https://github.com/pedrofelipefroes/portfolio">GitHub repo</a>.
+      </small>
+    </div>
+  </footer>
+)
+
+export default Footer
