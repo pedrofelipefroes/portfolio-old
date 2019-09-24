82c7592ad1734d02c29bd344232bc83b60bfa683
Pedro Froes <pfroes@ac-pfroes.local>
2019-09-23 21:25:14 -0300

    Updating index page styling.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,21 @@
 import React from "react"
 
 import Layout from "../components/layout/layout"
-import SEO from "../components/seo"
+import SEO from "../components/seo/seo"
 import ArrowedLink from "../components/arrowed-link/arrowedLink"
 
 import "../styles/index.scss"
 
 const IndexPage = () => (
-  <Layout hasNav={false} >
+  <Layout hasNav={false}>
     <SEO title="Portfolio. " />
-    <header className="p-home__header l-container">
-      <div className="p-home__header__title">
-        <h1>
-          Design, Development, and Marketing should intertwine to create
-          powerful product experiences.
-        </h1>
-      </div>
-      <div className="p-home__header__headline">
-        <h2>
+    <header className="p-index__header l-container__content">
+      <h1 className="l-container__text--small">
+        Design, Development, and Marketing should intertwine to create powerful
+        product experiences.
+      </h1>
+      <div className="p-index__header__headline">
+        <h2 className="l-container__text--small">
           <strong>I am Froes</strong>, and I love to work in this intersection
           developing cohesive brands through comprehensive design systems.
         </h2>
