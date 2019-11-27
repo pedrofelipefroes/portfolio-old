fc84cc95b10ae4139d170168f5a9cc2c2975373d
pedrofelipefroes <pedrofelipefroes@gmail.com>
2019-11-27 11:23:15 -0300

    Minor update on Footer styling.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,14 @@ import React from "react"
 
 const Footer = () => (
   <footer className="c-footer">
-    <div className="l-container__content">
+    <div className="l-container__content--S">
       <section className="c-footer__links">
-        <p><a href="https://www.linkedin.com/in/froesdesign">LinkedIn</a></p>
-        <p><a href="mailto:hello@froes.design">Work Inquiries</a></p>
+        <p>
+          <a href="https://www.linkedin.com/in/froesdesign">LinkedIn</a>
+        </p>
+        <p>
+          <a href="mailto:hello@froes.design">Work Inquiries</a>
+        </p>
       </section>
       <section className="c-footer__disclaimer">
         <small>
