288600d3e5105b0b886ad68d61be73963707630d
pedrofelipefroes <pedrofelipefroes@gmail.com>
2019-12-16 11:57:53 -0300

    Updating ImageContainer component.

--- src/components/imageContainer.js
+++ src/components/imageContainer.js
@@ -1,24 +1,45 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const ImageContainer = ({ imageQuantity, children }) => {
+const ImageContainer = ({
+  caption,
+  children,
+  hasFullWidth,
+  hasMultipleImages,
+}) => {
   return (
     <figure
       className={
-        imageQuantity
-          ? "l-container__content--L c-image-container c-image-container--" +
-            imageQuantity
-          : "l-container__content--L c-image-container"
+        (hasFullWidth
+          ? "l-container__content--L "
+          : "l-container__content--S ") +
+        (hasMultipleImages
+          ? "c-image-container c-image-container--multiple"
+          : "c-image-container")
       }
     >
       {children}
+      {caption ? (
+        <figcaption className="c-image-container__caption">
+          <small className="l-container__text--S">{caption}</small>
+        </figcaption>
+      ) : (
+        <></>
+      )}
     </figure>
   )
 }
 
 ImageContainer.propTypes = {
+  caption: PropTypes.string,
   children: PropTypes.node.isRequired,
-  imageQuantity: PropTypes.string,
+  hasFullWidth: PropTypes.bool,
+  hasMultipleImages: PropTypes.string,
+}
+
+ImageContainer.defaultProps = {
+  hasFullWidth: false,
+  hasMultipleImages: false,
 }
 
 export default ImageContainer
