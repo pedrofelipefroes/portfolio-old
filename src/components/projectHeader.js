f06096967c3e45f6c43f4c6f523d0812783456f6
pedrofelipefroes <pedrofelipefroes@gmail.com>
2019-12-16 11:59:16 -0300

    Updating ProjectHeader.

--- src/components/projectHeader.js
+++ src/components/projectHeader.js
@@ -3,38 +3,27 @@ import React from "react"
 
 const ProjectHeader = ({ children, projectData }) => {
   return (
-    <header>
+    <header className="c-project-header">
       <figure className="c-project-header__img-backdrop">
         <div className="l-container__content--S">{children}</div>
       </figure>
-      <div className="c-project-header l-container__content--S">
-        <h1>{projectData.title}</h1>
-        <h3 className="l-container__text--L">{projectData.subtitle}</h3>
-        <section className="c-project-header__section">
-          <h2>Context</h2>
-          <div className="c-project-header__section__content">
-            {projectData.context.map(item => (
-              <div className="c-project-header__section__content__item">
-                <h3 className="highlighted-type">
-                  <strong>{item.title}</strong>
-                </h3>
-                <p>{item.content}</p>
-              </div>
-            ))}
-          </div>
-        </section>
-        <section className="c-project-header__section">
-          <h2>Process</h2>
-          <div className="c-project-header__section__content">
-            {projectData.process.map(item => (
-              <div className="c-project-header__section__content__item">
-                <h3 className="highlighted-type">
-                  <strong>{item.title}</strong>
-                </h3>
-                <p>{item.content}</p>
-              </div>
-            ))}
-          </div>
+      <section className="c-project-header__title">
+        <div className="l-container__content--S">
+          <h1 className="l-container__text--M">{projectData.title}</h1>
+          <h3 className="l-container__text--M">{projectData.subtitle}</h3>
+        </div>
+      </section>
+
+      <div className="l-container__content--S">
+        <section className="c-project-header__details">
+          {projectData.details.map(item => (
+            <div className="c-project-header__details__content">
+              <small>
+                <strong>{item.title}</strong>
+              </small>
+              <small>{item.content}</small>
+            </div>
+          ))}
         </section>
       </div>
     </header>
