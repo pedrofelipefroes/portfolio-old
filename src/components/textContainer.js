5bbd354af2538293b42ce8caf7283fef6a4a2035
pedrofelipefroes <pedrofelipefroes@gmail.com>
2019-12-16 11:58:45 -0300

    Updating TextContainer.

--- src/components/textContainer.js
+++ src/components/textContainer.js
@@ -1,25 +1,25 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const TextContainer = ({ alignTo, children }) => {
+const TextContainer = ({ children, header, isHighlighted }) => {
   return (
     <div
       className={
-        "l-container__content--S c-text-container c-text-container--" + alignTo
+        isHighlighted
+          ? "l-container__content--S c-text-container c-text-container--highlighted"
+          : "l-container__content--S c-text-container"
       }
     >
+      <h2 className="highlighted-type c-text-container__header">{header}</h2>
       <p className="l-container__text--M">{children}</p>
     </div>
   )
 }
 
 TextContainer.propTypes = {
-  alignTo: PropTypes.string,
   children: PropTypes.node.isRequired,
-}
-
-TextContainer.defaultProps = {
-  alignTo: "center",
+  header: PropTypes.string,
+  isHighlighted: PropTypes.bool,
 }
 
 export default TextContainer
