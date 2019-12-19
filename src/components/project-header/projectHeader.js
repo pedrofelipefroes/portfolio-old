06c262c71281ad4e3e8fb2b176970a5e88ce93e6
pedrofelipefroes <pedrofelipefroes@gmail.com>
2019-12-19 11:46:55 -0300

    Updating ProjectHeader component and styling.

--- src/components/project-header/projectHeader.js
+++ src/components/project-header/projectHeader.js
@@ -1,27 +1,32 @@
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./project-header.scss"
+
 const ProjectHeader = ({ children, projectData }) => {
   return (
     <header className="c-project-header">
-      <figure className="c-project-header__img-backdrop">
-        <div className="l-container__content--S">{children}</div>
-      </figure>
-      <section className="c-project-header__title">
-        <div className="l-container__content--S">
-          <h1 className="l-container__text--M">{projectData.title}</h1>
-          <h3 className="l-container__text--M">{projectData.subtitle}</h3>
-        </div>
+      <section className="c-project-header__title l-container__content--S">
+        <h1 className="l-container__text--M">{projectData.title}</h1>
+        <h3 className="l-container__text--M u-color-type-variant">
+          {projectData.subtitle}
+        </h3>
       </section>
-
+      <figure className="c-project-header__cover">
+        <div className="c-project-header__cover__content">{children}</div>
+      </figure>
       <div className="l-container__content--S">
         <section className="c-project-header__details">
           {projectData.details.map(item => (
             <div className="c-project-header__details__content">
               <small>
-                <strong>{item.title}</strong>
+                <em>
+                  <strong className="u-color-type-variant">
+                    {item.title}:{" "}
+                  </strong>
+                  {item.content}
+                </em>
               </small>
-              <small>{item.content}</small>
             </div>
           ))}
         </section>
