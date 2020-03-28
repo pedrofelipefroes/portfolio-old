b1b6089a69769b8de52431e75672c6d8aebb4079
pedrofelipefroes <pedrofelipefroes@gmail.com>
2020-03-28 17:07:53 -0300

    Updating Nav styling and functionality.

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,56 +1,112 @@
-import PropTypes from "prop-types"
-import React from "react"
-
-import NavigationLink from "../navigation-link/navigation-link"
+import { Link } from "gatsby"
+import React, { Component } from "react"
+import classnames from "classnames"
 
 import "./nav.scss"
 
-const Nav = ({ hasAbout, hasShowcase, isOnDark }) => (
-  <nav className={isOnDark ? "c-nav c-nav--on-dark" : "c-nav"}>
-    <div className="u-content-container--XL">
-      <h4>
-        <strong>
-          <NavigationLink
-            className="c-nav__link-to-home"
+class Nav extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      itemsAreVisible: false,
+      navIsOnDark: props.isOnDark,
+      navIsVisible: true,
+      previousScrollPosition: window.pageYOffset,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const { previousScrollPosition } = this.state
+    const currentScrollPosition = window.pageYOffset
+    const navIsVisible =
+      previousScrollPosition > currentScrollPosition ||
+      currentScrollPosition <= 0
+
+    this.setState({
+      itemsAreVisible: false,
+      navIsVisible,
+      previousScrollPosition: currentScrollPosition,
+    })
+  }
+
+  handleClick = () => {
+    this.setState({
+      itemsAreVisible: !this.state.itemsAreVisible,
+    })
+  }
+
+  render() {
+    return (
+      <nav
+        className={classnames("c-nav", {
+          "c-nav--on-dark": this.state.navIsOnDark,
+          "c-nav--hidden": !this.state.navIsVisible,
+        })}
+      >
+        <div className="u-content-container--XL">
+          <Link
             to="/"
-            label=".design"
-          />
-        </strong>
-      </h4>
-      <div className="c-nav__items">
-        {/* {hasShowcase ? (
-          <h4>
-            <NavigationLink
-              className="c-nav__item"
-              to="/showcase"
-              label="Showcase"
-            />
-          </h4>
-        ) : (
-          <h4 className="c-nav__item c-nav__item--selected">Showcase</h4>
-        )} */}
-        {hasAbout ? (
-          <h4>
-            <NavigationLink className="c-nav__item" to="/about" label="About" />
-          </h4>
-        ) : (
-          <h4 className="c-nav__item c-nav__item--selected">About</h4>
-        )}
-      </div>
-    </div>
-  </nav>
-)
-
-Nav.propTypes = {
-  hasAbout: PropTypes.bool,
-  hasShowcase: PropTypes.bool,
-  isOnDark: PropTypes.bool,
-}
-
-Nav.defaultProps = {
-  hasAbout: true,
-  hasShowcase: true,
-  isOnDark: false,
+            className={classnames("highlight c-nav__home-url", {
+              "c-nav__home-url--hidden": this.state.itemsAreVisible,
+            })}
+          >
+            Froes.design
+          </Link>
+          <div className="c-nav__items">
+            <div
+              className={classnames("c-nav__items__links", {
+                "c-nav__items__links--hidden--S": !this.state.itemsAreVisible,
+              })}
+            >
+              <Link to="/showcase" className="highlight">
+                Showcase
+              </Link>
+              <Link to="/about" className="highlight">
+                About
+              </Link>
+            </div>
+            <svg
+              onClick={this.handleClick}
+              className={classnames("c-nav__arrow", {
+                "c-nav__arrow--inverted": this.state.itemsAreVisible,
+              })}
+              width="8px"
+              height="24px"
+              viewBox="0 0 8 24"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <rect
+                  width="9"
+                  height="2"
+                  x="0"
+                  y="0"
+                  transform="translate(0, 11) rotate(45) translate(-13, -12.5) translate(10, 7.5)"
+                />
+                <rect
+                  width="2"
+                  height="9"
+                  x="7"
+                  y="0"
+                  transform="translate(0, 11) rotate(45) translate(-13, -12.5) translate(10, 7.5)"
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default Nav
