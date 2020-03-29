9c952156d0368bd7f35a5b1e99a0d480706063e6
pedrofelipefroes <pedrofelipefroes@gmail.com>
2020-03-29 18:21:33 -0300

    Updating Footer.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,30 +1,69 @@
-import React from "react"
+import classnames from "classnames"
+import Fade from "react-reveal/Fade"
+import PropTypes from "prop-types"
+import React, { Component } from "react"
 
 import "./footer.scss"
 
-const Footer = () => (
-  <footer className="c-footer">
-    <div className="u-content-container--XL">
-      <section className="c-footer__links">
-        <h3>
-          <a href="https://www.linkedin.com/in/froesdesign">LinkedIn</a>
-        </h3>
-        <h3>
-          <a href="mailto:hello@froes.design">Work Inquiries</a>
-        </h3>
-      </section>
-      <section className="c-footer__copyright">
-        <small className="u-color-type-variant">
-          This site was fully designed and developed by me. Check out its{" "}
-          <a href="https://github.com/pedrofelipefroes/portfolio">
-            GitHub repo
-          </a>
-          .
-        </small>
-        <small className="u-color-type-variant">© 2020 Froes.</small>
-      </section>
-    </div>
-  </footer>
-)
+class Footer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      footerIsOnDark: props.isOnDark,
+    }
+  }
+
+  render() {
+    return (
+      <footer
+        className={classnames("c-footer", {
+          "c-footer--on-dark": this.state.footerIsOnDark,
+        })}
+      >
+        <div className="u-content-container--XL">
+          <div>
+            <Fade bottom>
+              <p>
+                <strong>
+                  Mail me at{" "}
+                  <a href="mailto:hello@froes.design">hello@froes.design</a>
+                </strong>
+              </p>
+            </Fade>
+            <Fade bottom>
+              <p>
+                <strong>
+                  Reach me on{" "}
+                  <a href="https://www.linkedin.com/in/froesdesign">
+                    linkedin.com/in/froesdesign
+                  </a>
+                </strong>
+              </p>
+            </Fade>
+          </div>
+          <div className="c-footer__copyright">
+            <small className="caption-2">
+              This site was fully designed and developed by me. Check out its{" "}
+              <a href="https://github.com/pedrofelipefroes/portfolio">
+                GitHub repo
+              </a>
+              .
+            </small>
+            <small className="caption-2">© 2020 Froes.</small>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
+
+Footer.propTypes = {
+  isOnDark: PropTypes.bool,
+}
+
+Footer.defautProps = {
+  isOnDark: false,
+}
 
 export default Footer
