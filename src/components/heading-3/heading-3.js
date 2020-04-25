73921fe62aa59206b52d12459364a92af42fb3e6
pedrofelipefroes <pedrofelipefroes@gmail.com>
2020-04-25 20:08:03 -0300

    Updating styling on H3 and P components.

--- src/components/heading-3/heading-3.js
+++ src/components/heading-3/heading-3.js
@@ -2,11 +2,9 @@ import Fade from "react-reveal/Fade"
 import PropTypes from "prop-types"
 import React from "react"
 
-import "./heading-3.scss"
-
 const Heading3 = ({ children }) => {
   return (
-    <div className="c-heading-3 u-content-container--L u-text-container">
+    <div className="u-content-container--L u-text-container">
       <Fade delay={640}>
         <h3>{children}</h3>
       </Fade>
