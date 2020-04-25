b0c782ab4d58121c31dd92043c489394124008a2
pedrofelipefroes <pedrofelipefroes@gmail.com>
2020-04-25 17:04:33 -0300

    Updating ProjectHeader.

--- src/components/project-header/project-header.js
+++ src/components/project-header/project-header.js
@@ -8,26 +8,65 @@ class ProjectHeader extends Component {
     super(props)
 
     this.state = {
-      coverHeight: 0,
+      titlePaddingBottom: 0,
     }
 
     this.cover = React.createRef()
-    this.title = React.createRef()
+    this.frontTitle = React.createRef()
+    this.frontTitleH1 = React.createRef()
+    this.backTitleH1 = React.createRef()
+
     this.handleScroll = this.handleScroll.bind(this)
   }
 
   componentDidMount() {
     window.addEventListener("scroll", this.handleScroll)
-
     this.setState({
-      coverHeight: this.cover.current.clientHeight,
+      titlePaddingBottom: Number(
+        getComputedStyle(this.frontTitleH1.current).paddingBottom.split("px")[0]
+      ),
     })
+  }
+
+  setParallax(parallaxRate, scrolledToTop) {
+    this.cover.current.style.top = -scrolledToTop * parallaxRate + "px"
+    this.frontTitle.current.style.clipPath =
+      "inset(0 0 " +
+      scrolledToTop * (parallaxRate / (this.cover.current.clientHeight / 100)) +
+      "% 0)"
+  }
+
+  setTransformValue(
+    currentPos,
+    finalPos,
+    finalScale,
+    initialPos,
+    initialScale
+  ) {
+    return (
+      initialScale +
+      ((currentPos - initialPos) * (finalScale - initialScale)) /
+        (finalPos - initialPos)
+    )
   }
 
   handleScroll() {
     let scrolledToTop = window.pageYOffset
-    this.cover.current.style.top = -scrolledToTop * 0.48 + "px"
-    this.title.current.style.top = -scrolledToTop * 0.64 + "px"
+    let scrollRate = 0.64
+
+    this.setParallax(scrollRate, scrolledToTop)
+
+    if (scrolledToTop <= this.cover.current.clientHeight * scrollRate) {
+      let newTitlePaddingBottom =
+        this.setTransformValue(
+          scrolledToTop,
+          this.cover.current.clientHeight * scrollRate,
+          0,
+          0,
+          this.state.titlePaddingBottom
+        ) + "px"
+      this.frontTitleH1.current.style.paddingBottom = this.backTitleH1.current.style.paddingBottom = newTitlePaddingBottom
+    }
   }
 
   render() {
@@ -44,27 +83,33 @@ class ProjectHeader extends Component {
           <source src={this.props.coverVideo} type="video/mp4" />
         </video>
         <div
-          ref={this.title}
-          style={{ paddingTop: this.state.coverHeight + "px" }}
+          className="c-project-header__title c-project-header__title--front"
+          ref={this.frontTitle}
         >
-          <div className="c-project-header__title u-content-container--L">
-            <h1>{this.props.title}</h1>
+          <div className="u-content-container--L">
+            <h1 ref={this.frontTitleH1}>{this.props.title}</h1>
           </div>
-          <div className="c-project-header__details-and-subtitle u-content-container--L">
-            <div className="c-project-header__details">
-              {this.props.details.map((item, index) => (
-                <div className="c-project-header__details__content">
-                  <small>{["Activity", "Client", "Duration"][index]}</small>
-                  <small>
-                    <em>{item}</em>
-                  </small>
-                </div>
-              ))}
-            </div>
-            <div className="c-project-header__subtitle">
-              <h3>{this.props.subtitle}</h3>
-            </div>
+        </div>
+        <div className="c-project-header__title c-project-header__title--back u-content-container--L">
+          <h1 ref={this.backTitleH1}>{this.props.title}</h1>
+        </div>
+        <div className="c-project-header__subtitle u-content-container--L">
+          <h3>{this.props.subtitle}</h3>
+        </div>
+        <div className="u-content-container--L">
+          <div className="c-project-header__details">
+            {this.props.details.map((item, index) => (
+              <div className="c-project-header__details__item" key={item}>
+                <small>{["Activity", "Client", "Duration"][index]}</small>
+                <small>
+                  <em>{item}</em>
+                </small>
+              </div>
+            ))}
           </div>
+        </div>
+        <div className="u-content-container--L">
+          <hr className="c-project-header__divider" />
         </div>
       </header>
     )
@@ -72,7 +117,8 @@ class ProjectHeader extends Component {
 }
 
 ProjectHeader.propTypes = {
-  cover: PropTypes.node.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  coverVideo: PropTypes.string.isRequired,
   details: PropTypes.array.isRequired,
   subtitle: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
