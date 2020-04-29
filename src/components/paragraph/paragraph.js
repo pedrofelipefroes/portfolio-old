3c0bb0ef67b0aa0301d0daf6d72a97e421f0f76b
pedrofelipefroes <pedrofelipefroes@gmail.com>
2020-04-28 22:03:10 -0300

    Updating Paragraph component.

--- src/components/paragraph/paragraph.js
+++ src/components/paragraph/paragraph.js
@@ -2,11 +2,9 @@ import Fade from "react-reveal/Fade"
 import PropTypes from "prop-types"
 import React from "react"
 
-import "./paragraph.scss"
-
 const Paragraph = ({ children }) => {
   return (
-    <div className="c-paragraph u-content-container--L u-text-container">
+    <div className="u-content-container--L u-text-container">
       <Fade delay={640}>
         <p>{children}</p>
       </Fade>
