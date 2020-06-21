037a972327ceb4ec151790ed3f4d5f133191975f
pedrofelipefroes <pedrofelipefroes@gmail.com>
2020-06-21 11:05:01 -0300

    Adding ProjectSummary component.

---
+++ src/components/project-summary/project-summary.js
@@ -0,0 +1,80 @@
+import Fade from "react-reveal/Fade"
+import PropTypes from "prop-types"
+import React from "react"
+
+import "./project-summary.scss"
+
+const ProjectSummary = ({ outline, process, results, role }) => {
+  return (
+    <section className="c-project-summary u-content-container--L">
+      <div className="c-project-summary__intro">
+        <div className="c-project-summary__role">
+          {role.map(item => (
+            <Fade delay={640}>
+              <small key={item}>{item}</small>
+            </Fade>
+          ))}
+        </div>
+        <div className="c-project-summary__outline-and-results">
+          <div className="c-project-summary__outline">
+            {outline.map((item, index) => (
+              <div className="c-project-summary__outline__item" key={item}>
+                <Fade delay={640}>
+                  <h3>{["Challenge", "Solution", "Results"][index]}</h3>
+                  <p>{item}</p>
+                </Fade>
+              </div>
+            ))}
+          </div>
+          <ul className="c-project-summary__results">
+            {results.map(item => (
+              <li key={item}>
+                <Fade delay={640}>
+                  <p>
+                    <strong>{item[0]}</strong>
+                  </p>
+                  <small className="u-font-weight--normal">{item[1]}</small>
+                </Fade>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <h2 className="c-project-summary__process__header">
+        Solution Process <br />
+        &amp; Deliverables
+      </h2>
+      <div className="c-project-summary__process">
+        {process.map((item, index) => (
+          <Fade delay={640 + index * 100}>
+            <div
+              key={item}
+              className={
+                `c-project-summary__process__item c-project-summary__process__item--` +
+                (index + 1)
+              }
+            >
+              <h3>{index + 1}</h3>
+              <div className="c-project-summary__process__item__header">
+                <h3>{item[0]}</h3>
+                <small className="u-color--dark-grey">
+                  Solution Stage {index + 1}
+                </small>
+              </div>
+              <small className="u-font-weight--normal">{item[1]}</small>
+            </div>
+          </Fade>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+ProjectSummary.propTypes = {
+  outline: PropTypes.array.isRequired,
+  process: PropTypes.array.isRequired,
+  results: PropTypes.array,
+  role: PropTypes.array.isRequired,
+}
+
+export default ProjectSummary
